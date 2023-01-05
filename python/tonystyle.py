4cb1162cadeeea4a35238fbe90605cc7465b6957
andradebru <brunansandrade@gmail.com>
2023-01-05 12:53:31 -0300

    corrects function update

--- python/tonystyle.py
+++ python/tonystyle.py
@@ -1,3 +1,4 @@
+# game of life rules
 # A live cell dies if it has fewer than two live neighbors.
 # A live cell with two or three live neighbors lives on to the next generation.
 # A live cell with more than three live neighbors dies.
@@ -5,42 +6,50 @@
 
 def init_matrix():
     return [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
 ]
 
 matrix = init_matrix()
-
-def count_live_neighbours(matrix, i, j):
-    return 0
 
 def print_matrix(matrix):
     D = {0: '-', 1: 'O'}
     for i in range(len(matrix)):
         print(''.join([D[e] for e in matrix[i]]))
 
-def update_matrix():
+def count_alive_neighbours(matrix, i, j):
+    possible_neighbour_position = [
+            (-1,-1), (-1,0), (-1,1), # (o--) (-o-) (--o)
+            (0,-1), (0,1),
+            (1,-1), (1,0), (1,1)
+        ]
+    neighbour_position = [
+        (i+pnp[0], j+pnp[1])
+        for pnp in possible_neighbour_position
+        if 0 <= i+pnp[0] < len(matrix)
+        and 0 <= j+pnp[1] < len(matrix[0])
+    ]
+    return len([1 for np in neighbour_position if matrix[np[0]][np[1]] == 1])
+
+def update_matrix(matrix):
     new_matrix = [[0 for e in line] for line in matrix]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            count_live_neighbours = count_live_neighbours(matrix, i, j)
+            alive_neighbours = count_alive_neighbours(matrix, i, j)
             if matrix[i][j] == 1:
-                if count_live_neighbours == 0 or count_live_neighbours == 1:
+                if alive_neighbours == 0 or alive_neighbours == 1:
                     new_matrix[i][j] = 0
-                elif count_live_neighbours >= 4:
+                elif alive_neighbours >= 4:
                     new_matrix[i][j] = 0
                 else:
                     new_matrix[i][j] = 1
             else:
-                if count_live_neighbours == 3:
+                if alive_neighbours == 3:
                     new_matrix[i][j] = 1
                 else:
                     new_matrix[i][j] = 0
     return new_matrix
-
-def update_matrix():
-    return 0
 
 while True:
     print_matrix(matrix)
