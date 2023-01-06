3605b4d66b68ba46fca384031617fba0a8781817
andradebru <brunansandrade@gmail.com>
2023-01-06 13:52:35 -0300

    importando certinho

--- vuelife/src/App.vue
+++ vuelife/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LifeGame/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import LifeGame from './components/LifeGame.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LifeGame
   }
 }
 </script>
